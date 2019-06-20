0d8bda4ba80404c213ef4e316e820d4c67049bd7
Chad Dunlap <chad-dunlap@hotmail.com>
2019-06-20 12:26:16 -0400

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Chad Dunlap on 6/20/19.
+//  Copyright © 2019 Chad Dunlap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

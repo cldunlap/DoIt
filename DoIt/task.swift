862a9b4ea58604a4495bc5673d47fced64a61843
Chad Dunlap <chad-dunlap@hotmail.com>
2019-07-16 16:34:43 -0400

    no message

---
+++ DoIt/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Chad Dunlap on 6/21/19.
+//  Copyright © 2019 Chad Dunlap. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = "name"
+    var important = false
+}
